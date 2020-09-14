f9e6aefcccc3ff230503f16e350fe155a129a06c
fbado66 <fbado66@gmail.com>
2020-09-14 10:11:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let firstToRemove = document.getElementById('main');
+firstToRemove.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.innerHTML = "Franklin is the champion";
